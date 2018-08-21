d360dae02f5dd28abb46fe39baf65c69aa17bdd8
Brian Avlund <brian.avlund@skat.dk>
2018-08-21 14:45:24 +0200

    Added buyer call to webservice

---
+++ omregistrering/AcquireResponse.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace omregistrering
+{
+    public class AcquireResponse
+    {
+        public String licensePlate { get; set; }
+    }
+}
--- omregistrering/BayerFetchRequest.xaml.cs
+++ omregistrering/BayerFetchRequest.xaml.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace omregistrering
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class BayerFetchRequest : ContentPage
 	{
 		public BayerFetchRequest ()
@@ -19,8 +18,35 @@ namespace omregistrering
 
         private async void OnEntryCompleted(object sender, EventArgs e)
         {
-            //Connect to webservice send 'Entry.text' and retrieve the vehicle id            
-            await Navigation.PushAsync(new ListVehicleDetails());
+            try
+            {
+                var acquireResponse = callRestService(((Entry)sender).Text);
+
+                if (acquireResponse != null)
+                {
+                    await Navigation.PushAsync(new ListVehicleDetails());
+                }
+            } catch(Exception ex) {
+                Console.WriteLine("fejl: " + ex.Message);
+            }
+        }
+
+        private AcquireResponse callRestService(String handoffId)
+        {
+            HttpClient client = new HttpClient();
+            var uri = new Uri("http://10.105.112.115:8080/acquire/initiate/ww00007");
+
+            var values = new Dictionary<string, string> {{ "handoffId", handoffId }};
+            //var httpContent = new FormUrlEncodedContent(values);
+
+            var httpContent = new StringContent("{ 'handoffId': '" + handoffId + "'}", Encoding.UTF8, "application/json");
+
+            var response = client.PostAsync(uri, httpContent).Result;
+
+            response.EnsureSuccessStatusCode();
+
+            var content = response.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<AcquireResponse>(content);
         }
     }
 }
